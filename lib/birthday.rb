b1dd2b09accd81c635279c81ad5b7861d6ffbd69
Marcus Moreno <legendary.1198@gmail.com>
2020-07-14 19:26:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def appropriate_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   
   birthday_kids.each do |kids_name, age|
     
